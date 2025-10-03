656b6ff8113893e2afbe42b9681f8c853d901cca
mathias9939 <mathias9939@gmail.com>
2025-10-02 23:03:38 -0500

    Update servicios.h

--- Proyecto_Banco/servicios.h
+++ Proyecto_Banco/servicios.h
@@ -2,12 +2,10 @@
 #include <iostream>
 #include <string>
 #include <functional>
+#include "credito.h"
 
 using namespace std;
 
-
-template <typename TCodigo, typename TSaldo, typename TTitular, typename TCuenta, typename TFApertura>
-class Servicios;
 
 ////////////////////////////////////////////////////
 // CLASE BASE SERVICIOS
@@ -62,81 +60,9 @@ public:
     void setTitular(TTitular nuevo_titular) { titular = nuevo_titular; }
 };
 
-
-////////////////////////////////////////////CREDITO////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-// CLASE CREDITO - Integrada en el sistema de Servicios
-////////////////////////////////////////////////////
-class Credito {
-protected:
-    double monto_prestamo;
-    int plazo_meses;
-    double tasa_interes;
-    double saldo_pendiente;
-
-public:
-    // Constructor
-    Credito(double monto, int plazo, double tasa)
-        : monto_prestamo(monto), plazo_meses(plazo), tasa_interes(tasa), saldo_pendiente(monto) {
-    }
-
-    // Destructor virtual
-    virtual ~Credito() {}
-
-    // Métodos
-    virtual void desembolsar() {
-        cout << "Desembolsando crédito por: $" << monto_prestamo << endl;
-        saldo_pendiente = monto_prestamo;
-    }
-
-    virtual void pagarCuota(double monto) {
-        if (monto <= saldo_pendiente) {
-            saldo_pendiente -= monto;
-            cout << "Pago realizado. Saldo pendiente: $" << saldo_pendiente << endl;
-        }
-        else {
-            cout << "Monto excede el saldo pendiente" << endl;
-        }
-    }
-
-    virtual double calcularInteres() const {
-        return saldo_pendiente * tasa_interes;
-    }
-
-    virtual void generarCronogramaPagos() const {
-        cout << "=== CRONOGRAMA DE PAGOS ===" << endl;
-        cout << "Monto total: $" << monto_prestamo << endl;
-        cout << "Plazo: " << plazo_meses << " meses" << endl;
-        cout << "Tasa de interés: " << (tasa_interes * 100) << "%" << endl;
-
-        double cuota = monto_prestamo / plazo_meses;
-        double saldo = monto_prestamo;
-
-        for (int i = 1; i <= plazo_meses; i++) {
-            double interes = saldo * tasa_interes;
-            double amortizacion = cuota - interes;
-            saldo -= amortizacion;
-
-            cout << "Mes " << i << ": Cuota $" << cuota
-                << " (Interés: $" << interes << ", Amortización: $" << amortizacion << ")" << endl;
-        }
-    }
-
-    // Getters
-    double getMontoPrestamo() const { return monto_prestamo; }
-    int getPlazoMeses() const { return plazo_meses; }
-    double getTasaInteres() const { return tasa_interes; }
-    double getSaldoPendiente() const { return saldo_pendiente; }
-
-    // Setters
-    void setTasaInteres(double tasa) { tasa_interes = tasa; }
-    void setSaldoPendiente(double saldo) { saldo_pendiente = saldo; }
-};
-
 ////////////////////////////////////////////////////
 // CLASE SERVICIO_CREDITO - Servicio especializado en créditos
 ////////////////////////////////////////////////////
-
 template <typename TCodigo, typename TSaldo, typename TTitular, typename TCuenta, typename TFApertura>
 class ServicioCredito : public Servicios<TCodigo, TSaldo, TTitular, TCuenta, TFApertura> {
 private:
@@ -202,106 +128,5 @@ public:
     void setCreditoAsociado(Credito* credito) {
         if (credito_asociado) delete credito_asociado;
         credito_asociado = credito;
-    }
-};
-//////////////////////////////////////////////////////FIN CREDIDO//////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////
-// LISTA ENLAZADA PARA GESTIÓN DE SERVICIOS ESTO LO USE PARA LOS TIPOS DE CREDITO
-////////////////////////////////////////////////////
-template <typename T>
-class Nodo {
-public:
-    T dato;
-    Nodo<T>* siguiente;
-
-    Nodo(T valor) : dato(valor), siguiente(nullptr) {}
-};
-
-template <typename T>
-class ListaEnlazada {
-private:
-    Nodo<T>* cabeza;
-    int tamaño;
-
-public:
-    // Constructor
-    ListaEnlazada() : cabeza(nullptr), tamaño(0) {}
-
-    // Destructor
-    ~ListaEnlazada() {
-        limpiar();
-    }
-
-    // Método 1: Insertar al final
-    void insertarFinal(T valor) {
-        Nodo<T>* nuevoNodo = new Nodo<T>(valor);
-
-        if (!cabeza) {
-            cabeza = nuevoNodo;
-        }
-        else {
-            Nodo<T>* actual = cabeza;
-            while (actual->siguiente) {
-                actual = actual->siguiente;
-            }
-            actual->siguiente = nuevoNodo;
-        }
-        tamaño++;
-    }
-
-    // Método 2: Filtrar con lambda
-    ListaEnlazada<T> filtrar(function<bool(T)> criterio) {
-        ListaEnlazada<T> resultado;
-        Nodo<T>* actual = cabeza;
-
-        auto filtrarElementos = [&](Nodo<T>* actual, function<bool(T)> criterio) {
-            while (actual) {
-                if (criterio(actual->dato)) {
-                    resultado.insertarFinal(actual->dato);
-                }
-                actual = actual->siguiente;
-            }
-            };
-
-        filtrarElementos(actual, criterio);
-        return resultado;
-    }
-
-    // Método 3: Aplicar función a todos los elementos
-    void transformar(function<void(T&)> funcion) {
-        Nodo<T>* actual = cabeza;
-        while (actual) {
-            funcion(actual->dato);
-            actual = actual->siguiente;
-        }
-    }
-
-    // Métodos auxiliares
-    void mostrar() const {
-        Nodo<T>* actual = cabeza;
-        while (actual) {
-            cout << actual->dato << " -> ";
-            actual = actual->siguiente;
-        }
-        cout << "NULL" << endl;
-    }
-
-    int getTamaño() const {
-        return tamaño;
-    }
-
-    bool estaVacia() const {
-        return cabeza == nullptr;
-    }
-
-    void limpiar() {
-        while (cabeza) {
-            Nodo<T>* temp = cabeza;
-            cabeza = cabeza->siguiente;
-            delete temp;
-        }
-        tamaño = 0;
     }
 };
